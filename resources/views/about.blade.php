ff83a199bb6ef5972767ddca0c1d957bc141a6a6
rcslbaa <ifasmarani3125@gmail.com>
2026-03-03 23:05:27 +0700

    Fix gitignore dan upload semua kodingan

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-[#1a202c] overflow-hidden shadow-sm sm:rounded-lg border border-gray-700">
+                <div class="p-10 text-gray-100">
+                    <div class="space-y-2 text-lg font-medium">
+                        <div class="flex">
+                            <span class="w-32">Nama</span>
+                            <span>: Ifa Asmarani Rosalba</span>
+                        </div>
+                        <div class="flex">
+                            <span class="w-32">NIM</span>
+                            <span>: 20230140015</span>
+                        </div>
+                        <div class="flex">
+                            <span class="w-32">Program Studi</span>
+                            <span>: Teknologi Informasi</span>
+                        </div>
+                        <div class="flex">
+                            <span class="w-32">Hobi</span>
+                            <span>: Nonton</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
